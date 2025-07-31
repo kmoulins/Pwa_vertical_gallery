40ef37348dc7b5e18edc7c8699635c6a1163ebcb
kmoulins <k.moulins@zoho.com>
2025-08-01 01:57:52 +0200

    Update script.js, removed slash before sw

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function registerSw(){
 	console.log('hello world');
 	if('serviceWorker' in navigator){
 	console.log('sw available');
-		navigator.serviceWorker.register('/sw.js')
+		navigator.serviceWorker.register('sw.js')
 		.then(registration =>{console.log('sw registered', registration.scope);})
 		.catch(e=>{console.log('registration failed',e);});
 	}
